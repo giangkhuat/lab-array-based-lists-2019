46eeaff267fdf015f87cb8f4d2399d99d5fa7662
Khuat <khuatgia@cs.grinnell.edu>
2019-05-16 14:44:22 -0500

    may  review for final

--- src/SALExpt.java
+++ src/SALExpt.java
@@ -12,24 +12,38 @@ public class SALExpt {
     SimpleArrayList<String> fruits = new SimpleArrayList<String> ();
     ListIterator<String> it = (ListIterator<String>) fruits.iterator();
     
+  //  String tmp = "papapa";
     
 	it.add("banana");
-    it.add("papapa");
+    it.add("papaya");
     it.add("apple");
     it.add("boba");
-    
+    //pen.println(tmp.equals("papaya))
+    /*
+    while (it.hasPrevious()) {
+      String tmp = it.previous();
+      pen.println(tmp);
+      if (tmp.equals("papaya")) {
+        it.remove();
+      }
+    }
+    */
+   
     ListIterator<String> it2 = (ListIterator<String>) fruits.iterator();
-    pen.println("hello");
+    pen.println("_________");
     while (it2.hasNext()) {
-    	if (it2.next().equals("papaya")) {
+      String tmp = it2.next();
+      pen.println(tmp);
+    	if (tmp.equals("papaya")) {
     		it2.set("kungfu");
     	}
     }
-    
+    pen.println("_________");
     ListIterator<String> it3 = (ListIterator<String>) fruits.iterator();
     while (it3.hasNext()) {
     	pen.println(it3.next());
     }
+    
     
     
   } // main(String[]
